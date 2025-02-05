eb8d614ffc1d310818c410b7c6eee51fea5bbde3
G24AI1110 <G24AI1110@IITJ.ac.in>
2025-02-05 21:42:38 +0530

    Update VM1.py

--- VM1.py
+++ VM1.py
@@ -1,13 +1,12 @@
-### vm1_time_service.py 
 from flask import Flask
 from datetime import datetime
 
-app = Flask(_name_)
+app = Flask(__name__)
 
 @app.route('/time', methods=['GET'])
 def get_time():
     current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     return {"current_time": current_time}
 
-if _name_ == '_main_':
-    app.run(host= '192.168.31.136', port=5001)
+if __name__ == '__main__':
+    app.run(host='192.168.31.136', port=5001)
