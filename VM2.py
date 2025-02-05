ee14cb66b825a0ee6a23ad62f241d62a757aaeb3
G24AI1110 <G24AI1110@IITJ.ac.in>
2025-02-05 21:43:57 +0530

    Update VM2.py

--- VM2.py
+++ VM2.py
@@ -1,17 +1,13 @@
-VM 2: Random Number Service  
 
-This microservice will return a random number between 1 and 100. 
-
-# vm2_random_number_service.py
 from flask import Flask
 import random
 
-app = Flask(_name_)
+app = Flask(__name__)
 
 @app.route('/random', methods=['GET'])
 def get_random_number():
     random_number = random.randint(1, 100)
     return {"random_number": random_number}
 
-if _name_ == '_main_':
+if __name__ == '__main__':
     app.run(host='192.168.31.202', port=5002)
