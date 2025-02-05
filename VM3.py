6a8f8fc1a7d6f32a1c873fff76ed847b5c4e8b8f
G24AI1110 <G24AI1110@IITJ.ac.in>
2025-02-05 21:46:19 +0530

    Update VM3.py

--- VM3.py
+++ VM3.py
@@ -1,16 +1,11 @@
-VM 3: Greeting Service  
-
-This microservice will return a greeting message. 
-
 # vm3_greeting_service.py
 from flask import Flask
 
-app = Flask(_name_)
+app = Flask(__name__)
 
 @app.route('/greet', methods=['GET'])
 def greet():
     return {"message": "Hello from VM 3!"}
 
-if _name_ == '_main_':
+if __name__ == '__main__':
     app.run(host='192.168.32.18', port=5003)
-
