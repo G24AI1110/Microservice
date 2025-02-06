3782b37e456608960663298fb1f12a854d94585f
G24AI1110 <G24AI1110@IITJ.ac.in>
2025-02-06 09:13:38 +0530

    Update VM3.py

--- VM3.py
+++ VM3.py
@@ -1,12 +1,10 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
 @app.route('/greet', methods=['GET'])
 def greet():
-    return {"message": "Hello from VM 3!"}
+    return jsonify({"message": "Hello from VM 3!"})  # Use jsonify()
 
 if __name__ == '__main__':
-    # Use '0.0.0.0' to bind the app to all available network interfaces
-    # or use the actual local IP if you prefer
-    app.run(host='192.168.31.202', port=5003)
+    app.run(host='192.168.31.202', port=5003, debug=True)
