9f6fef131cd8e321b6f9ba43007f9a08acf9d4b3
G24AI1110 <G24AI1110@IITJ.ac.in>
2025-02-06 10:49:59 +0530

    Update VM2.py

--- VM2.py
+++ VM2.py
@@ -10,4 +10,4 @@ def get_random_number():
     return {"random_number": random_number}
 
 if __name__ == '__main__':
-    app.run(host='192.168.31.202', port=5002)
+    app.run(host='192.168.31.18', port=5002)
