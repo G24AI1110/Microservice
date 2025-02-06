5b32578264c8d31b5f630ca1b41aed1f60d22518
G24AI1110 <G24AI1110@IITJ.ac.in>
2025-02-06 10:48:26 +0530

    Update VM3.py

--- VM3.py
+++ VM3.py
@@ -7,4 +7,4 @@ def greet():
     return jsonify({"message": "Hello from VM 3!"})  # Use jsonify()
 
 if __name__ == '__main__':
-    app.run(host='192.168.31.202', port=5003, debug=True)
+    app.run(host='192.168.31.18', port=5003, debug=True)
